ad686fec0897bcdce6800582360fc326bfd03bb3
Michael Lesniak <mail@mlesniak.com>
2019-11-11 22:28:03 +0100

    Encrypt and decrypt password

--- main.go
+++ main.go
@@ -25,9 +25,19 @@ func main() {
 		fmt.Println(prime)
 	}
 
+	// Generate random password.
+	password := make([]byte, 32)
+	pwLen, err := rng.Read(password)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("--- Password")
+	fmt.Printf("%d: %v\n", pwLen, password)
+
 	// Encrypt password (for large messages) using public key.
 	fmt.Println("--- Encrypting")
-	secretMessage := []byte("Hello, world")
+	secretMessage := password
 	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rng, &pub, secretMessage, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -35,13 +45,12 @@ func main() {
 	}
 	fmt.Println(ciphertext)
 
-	// Decrypt using private key.
+	// Decrypt password using private key.
 	fmt.Println("--- Decrypting")
 	plaintext, err := rsa.DecryptOAEP(sha256.New(), nil, key, ciphertext, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	text := string(plaintext)
-	fmt.Println(text)
+	fmt.Println(plaintext)
 }
